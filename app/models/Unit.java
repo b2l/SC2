870647ee0c9b552490a88572acd595f95df266e3
blandine <blandine@aquarius.(none)>
2013-06-06 15:32:56 +0200

    unit model wip

---
+++ app/models/Unit.java
@@ -0,0 +1,23 @@
+package models;
+
+import java.io.File;
+
+public class Unit {
+	private String name;
+	private File picture;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public File getPicture() {
+		return picture;
+	}
+	public void setPicture(File picture) {
+		this.picture = picture;
+	}
+	
+
+}
