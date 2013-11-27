471e9abc36cf2bcca8b4d676863a99448f44163c
Nicolas MEDDA <b2l.powa@gmail.com>
2013-11-27 21:36:21 +0100

    Fix typo and test name

--- srv/tests/parserTest.js
+++ srv/tests/parserTest.js
@@ -1,9 +1,9 @@
 var expect = require('chai').expect;
+
 var FS = require('fs');
 var SC2DataParser = require('../app/lib/SC2DataParser');
 
 describe('Parser', function() {
-
 
     it(' should extract units name', function(done) {
         var filePath = FS.realpathSync('./resources/UnitData.xml');
@@ -12,7 +12,7 @@ describe('Parser', function() {
         parser.parse(filePath).then(function(data) {
             expect(data.Catalog.CUnit).to.have.length(288);
 
-            // Test that there is a Zergling
+            // Test that there is a oracle
             var oracle = data.Catalog.CUnit.filter(function(unit) {
                 return unit.$.id === "Oracle";
             });
